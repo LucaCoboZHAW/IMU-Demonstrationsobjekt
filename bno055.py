5c2d36ae3b3c73b4bc53b939c582e1e9c9fa38b8
LucaCoboZHAW <coboluc1@students.zhaw.ch>
2026-06-05 15:11:55 +0200

    Update bno055.py

--- bno055.py
+++ bno055.py
@@ -7,7 +7,7 @@ class BNO055:
         self.adress = adress
         self.bus = smbus2.SMBus(1)                              #Bus 1 (Pin3 & 5)
         self.bus.write_byte_data(self.adress, 0x3F,0x00)        #register 0x3F alle bits spez bit 7 auf 0 sezten für interne osxillator als clk_selected
-        self.bus.write_byte_data(self.adress, 0x3D,0x08)        #betreiben des sensor im IMU modus , ohne magnetometer
+        self.bus.write_byte_data(self.adress, 0x3D,0x08)        
         self.sleep_time = 0.1
 
 
